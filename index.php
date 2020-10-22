67e9e73f1fc3f87fc521669ecd64f680bb1ae735
Eliza <itzjusteliza@gmail.com>
2020-10-22 15:05:39 +0200

    Uppgift 5 & 6

--- index.php
+++ index.php
@@ -1,9 +1,84 @@
 <?php
 
-function myFunction($string)
+// Uppgift 1 & 2
+function myFunction($string, $siffra)
 {
-    echo $string;
+    echo "<h" . $siffra . ">" . $string . "<h" . $siffra . ">";
+}
+myFunction("Hello Kitchen", 1);
+
+
+// Uppgift 3
+class Food
+{
+    public $name;
+
+    function eat()
+    {
+        echo "Nu åts en" . $this->name;
+    }
 }
 
+$objekt = new Food();
+$objekt->eat('dadel');
 
-myFunction("<h3> En sträng. <h3>");
+
+// Uppgift 4 & 5
+class Sandwich extends Food
+{
+    private $kontrollLista = [
+        "cheese",
+        "ham",
+        "mustard",
+        "ketchup",
+        "fish"
+    ];
+
+    private $options = [];
+
+    function eat()
+    {
+        foreach ($this->options as $palagg => $styck) {
+            echo "<li>" . $styck . " " . $palagg;
+        }
+    }
+
+    function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    function get_order_options()
+    {
+        return $this->optionsList;
+    }
+
+    function set_options($optionsList)
+    {
+        foreach ($optionsList as $palagg => $styck) {
+            if (in_array($palagg, $this->kontrollLista)) {
+                $this->options[$palagg] = $styck;
+            }
+        }
+    }
+}
+
+$beställning = [
+    "cheese" => 1,
+    "fish" => "9"
+];
+
+$beställning2 = [
+    "cheese" => 2,
+    "ham" => "1"
+];
+
+$räkmacka = new Sandwich('räkmacka'); {
+    $ostmacka = new Sandwich("ostmacka");
+};
+
+$ostmacka->set_options($beställning2);
+$ostmacka->eat();
+
+$räkmacka->set_options($beställning);
+$räkmacka->eat();
